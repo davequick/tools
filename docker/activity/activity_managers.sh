0608bfbe7e01d4af97439839b1db9159e4fd9452
Dave Quick <dq@dq.gg>
2019-01-07 10:19:28 -0800

    checkpoint

--- docker/activity/activity_managers.sh
+++ docker/activity/activity_managers.sh
@@ -1,28 +1,56 @@
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
-ssh manager01.dq.gg -t docker run -d --restart=always --name activity7 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
 
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
-ssh manager02.dq.gg -t docker run -d --restart=always --name activity7 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
+docker node update --label-add gg.dq.availability_zone=A manager01.dq.gg
+docker node update --label-add gg.dq.availability_zone=B manager02.dq.gg
+docker node update --label-add gg.dq.availability_zone=C manager03.dq.gg
 
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity7 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
+docker node update --label-add gg.dq.availability_zone=A node01.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node02.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node03.dq.gg
+docker node update --label-add gg.dq.availability_zone=A node04.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node05.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node06.dq.gg
+docker node update --label-add gg.dq.availability_zone=A node07.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node08.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node09.dq.gg
+docker node update --label-add gg.dq.availability_zone=A node10.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node11.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node12.dq.gg
+docker node update --label-add gg.dq.availability_zone=A node13.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node14.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node15.dq.gg
+docker node update --label-add gg.dq.availability_zone=A node16.dq.gg
+docker node update --label-add gg.dq.availability_zone=B node17.dq.gg
+docker node update --label-add gg.dq.availability_zone=C node18.dq.gg
 
-ssh manager03.dq.gg -t docker run -d --restart=always --name activity7 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
+
+
+docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
+docker run -d --restart=always --name activity7 --cpuset-cpus 7 --cpus 0.01 davequick/image:activity
+
+docker run -d --restart=always --name healthcheck-local -p "6970:8080" davequick/image:activity
+
+
+docker node update --label-add gg.dq.storage="large" node01.dq.gg
+docker node update --label-add gg.dq.storage="large" node02.dq.gg
+docker node update --label-add gg.dq.storage="large" node03.dq.gg
+docker node update --label-add gg.dq.storage="large" node04.dq.gg
+docker node update --label-add gg.dq.storage="large" node05.dq.gg
+docker node update --label-add gg.dq.storage="medium" node06.dq.gg
+docker node update --label-add gg.dq.storage="medium" node07.dq.gg
+docker node update --label-add gg.dq.storage="medium" node08.dq.gg
+docker node update --label-add gg.dq.storage="medium" node09.dq.gg
+docker node update --label-add gg.dq.storage="medium" node10.dq.gg
+docker node update --label-add gg.dq.storage="medium" node11.dq.gg
+docker node update --label-add gg.dq.storage="medium" node12.dq.gg
+docker node update --label-add gg.dq.storage="medium" node13.dq.gg
+docker node update --label-add gg.dq.storage="medium" node14.dq.gg
+docker node update --label-add gg.dq.storage="medium" node15.dq.gg
+docker node update --label-add gg.dq.storage="small" node16.dq.gg
+docker node update --label-add gg.dq.storage="small" node17.dq.gg
+docker node update --label-add gg.dq.storage="small" node18.dq.gg
---
+++ docker/scripts/dockerize.md
@@ -0,0 +1,1 @@
+https://github.com/jwilder/dockerize has tools  to more easily dockerize things.
