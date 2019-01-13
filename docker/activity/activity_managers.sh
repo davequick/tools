3aead78a91f17c1fc3a92985160ce19087d8cd8b
Dave Quick <dq@dq.gg>
2019-01-13 01:48:04 -0800

    playing with all the docker stack options and learning as i go

--- docker/activity/activity_managers.sh
+++ docker/activity/activity_managers.sh
@@ -23,19 +23,6 @@ docker node update --label-add gg.dq.availability_zone=B node17.dq.gg
 docker node update --label-add gg.dq.availability_zone=C node18.dq.gg
 
 
-
-docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity
-docker run -d --restart=always --name activity7 --cpuset-cpus 7 --cpus 0.01 davequick/image:activity
-
-docker run -d --restart=always --name healthcheck-local -p "6970:8080" davequick/image:activity
-
-
 docker node update --label-add gg.dq.storage="large" node01.dq.gg
 docker node update --label-add gg.dq.storage="large" node02.dq.gg
 docker node update --label-add gg.dq.storage="large" node03.dq.gg
@@ -49,8 +36,23 @@ docker node update --label-add gg.dq.storage="medium" node10.dq.gg
 docker node update --label-add gg.dq.storage="medium" node11.dq.gg
 docker node update --label-add gg.dq.storage="medium" node12.dq.gg
 docker node update --label-add gg.dq.storage="medium" node13.dq.gg
-docker node update --label-add gg.dq.storage="medium" node14.dq.gg
-docker node update --label-add gg.dq.storage="medium" node15.dq.gg
-docker node update --label-add gg.dq.storage="small" node16.dq.gg
-docker node update --label-add gg.dq.storage="small" node17.dq.gg
-docker node update --label-add gg.dq.storage="small" node18.dq.gg
+docker node update --label-add gg.dq.storage="small" node14.dq.gg
+docker node update --label-add gg.dq.storage="small" node15.dq.gg
+docker node update --label-add gg.dq.storage="medium" node16.dq.gg
+docker node update --label-add gg.dq.storage="tiny" node17.dq.gg
+docker node update --label-add gg.dq.storage="medium" node18.dq.gg
+
+
+ansible -i inventory -f 20 all   -a "docker run -d --restart=always --name activity0 --cpuset-cpus 0 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 all   -a "docker run -d --restart=always --name activity1 --cpuset-cpus 1 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc4 -a "docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc4 -a "docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity2 --cpuset-cpus 2 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity3 --cpuset-cpus 3 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity4 --cpuset-cpus 4 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity5 --cpuset-cpus 5 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity6 --cpuset-cpus 6 --cpus 0.01 davequick/image:activity"
+ansible -i inventory -f 20 proc8 -a "docker run -d --restart=always --name activity7 --cpuset-cpus 7 --cpus 0.01 davequick/image:activity"
+
+ansible -i inventory all -a "docker run -d --restart=always --name healthcheck-local -p '6969:8080' davequick/image:healthcheck"
+
