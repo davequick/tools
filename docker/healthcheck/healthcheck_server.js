58a7888a407942283240fe52ff5f55ee0f679424
Dave Quick <dq@dq.gg>
2019-01-15 10:57:03 -0800

    trim status for public site

--- docker/healthcheck/healthcheck_server.js
+++ docker/healthcheck/healthcheck_server.js
@@ -4,18 +4,26 @@ const os = require("os");
 const http = require ("http");
 
 function info(req) {
+    // const info_format = [
+    //         [req.method, req.httpVersion, req.url],
+    //         ["On host:", os.hostname()],
+    //         ["NODE_ID",process.env.NODE_ID],
+    //         ["NODE_HOSTNAME",process.env.NODE_HOSTNAME],
+    //         ["SERVICE_ID",        process.env.SERVICE_ID],
+    //         ["SERVICE_NAME",      process.env.SERVICE_NAME],
+    //         ["SERVICE_LABELS",    process.env.SERVICE_LABELS],
+    //         ["SERVICE_DNSPEERS",  process.env.SERVICE_DNSPEERS],
+    //         ["TASK_ID",           process.env.TASK_ID],
+    //         ["TASK_NAME",         process.env.TASK_NAME],
+    //         ["TASK_SLOT",         process.env.TASK_SLOT],
+    //         []
+    //     ];
+
     const info_format = [
-            [req.method, req.httpVersion, req.url],
-            ["On host:", os.hostname()],
-            ["NODE_ID",process.env.NODE_ID],
+            ["OK"],
+            ["---"],
             ["NODE_HOSTNAME",process.env.NODE_HOSTNAME],
-            ["SERVICE_ID",        process.env.SERVICE_ID],
-            ["SERVICE_NAME",      process.env.SERVICE_NAME],
-            ["SERVICE_LABELS",    process.env.SERVICE_LABELS],
-            ["SERVICE_DNSPEERS",  process.env.SERVICE_DNSPEERS],
-            ["TASK_ID",           process.env.TASK_ID],
-            ["TASK_NAME",         process.env.TASK_NAME],
-            ["TASK_SLOT",         process.env.TASK_SLOT],
+            ["TASK_SLOT",    process.env.TASK_SLOT],
             []
         ];
 
@@ -23,6 +31,5 @@ function info(req) {
             .map(l=>l.join(" "))
             .join("\n");
 }
-
 http.createServer((req,res) => { res.end(info(req)); })
     .listen(8080, "0.0.0.0", ()=> {})
